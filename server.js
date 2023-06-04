6bc5925f881a4dcce34bb3b9f4bfc28066adf954
hirokitakagi <h.takagi@hakoreco.com>
2023-06-04 15:04:07 +0900

    server.jsの中身変更

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+// httpモジュールをrequireする
+const http = require("http");
+// ポート番号を指定
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  // 200のステータスコードとヘッダーを出力
+  res.writeHead(200, { "Content-Type": "text/html" });
+  // 出力するタグ
+  res.write("<h1>Hello World</h1>");
+  // 終わり
+  res.end();
+});
+
+// 表示する関数
+server.listen(port, () => {
+  console.log("Server running");
+});
